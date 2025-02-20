44cca921e40f3af5c48815422430892737403700
Javier Carchipulla S. <krchipullajavier@gmail.com>
2025-02-20 12:55:06 -0500

    seguridad - implementar cledenciales en un archivo .properties - implementamos la Properties - La clase Properties es una extensión de Hashtable diseñada para manejar pares de clave-valor en forma de cadena. - Se utiliza principalmente para cargar y almacenar configuraciones en aplicaciones Java, como archivos de propiedades (por ejemplo, config.properties).

--- src/main/java/configuration/CConexion.java
+++ src/main/java/configuration/CConexion.java
@@ -2,46 +2,53 @@ package configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Properties;
 import javax.swing.JOptionPane;
 
 public class CConexion {
     
     Connection conectar = null;
-    
-    String usuario ="root";
-    String contra = "12345";
-    String db = "dbstore";
-    String ip = "localhost";
-    String puerto = "3306";
-    String cadena = "Jdbc:mysql://"+ip+":"+puerto+"/"+db;
-    
+
     public Connection conectarme(){
         try {
+//            // Carga del archivo de propiedades
+//            Properties prop = new Properties();
+//            prop.load(getClass().getClassLoader().getResourceAsStream("configuration/config.properties"));
+
+//            // Lectura de propiedades
+//            String usuario = prop.getProperty("db.usuario");
+//            String contra = prop.getProperty("db.contra");
+//            String db = prop.getProperty("db.nombre");
+//            String ip = prop.getProperty("db.ip");
+//            String puerto = prop.getProperty("db.puerto");
+
+            String usuario ="root";
+            String contra = "12345";
+            String db = "dbstore";
+            String ip = "localhost";
+            String puerto = "3306";
+
+
+
+            String cadena = "jdbc:mysql://" + ip + ":" + puerto + "/" + db;
+
             Class.forName("com.mysql.jdbc.Driver");
             conectar = DriverManager.getConnection(cadena, usuario, contra);
-            JOptionPane.showMessageDialog(null, "Conexion correcta a la base de datos");   
+            JOptionPane.showMessageDialog(null, "Conexion correcta a la base de datos");
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "No se Conecto a la base de datos: "+ e.toString());
+            JOptionPane.showMessageDialog(null, "No se conectó a la base de datos: " + e.toString());
         }
         return conectar;
     }
-    
-    
-    
+
     public void cerrarConexion(){
         try {
             if (conectar != null && !conectar.isClosed()) {
                 conectar.close();
-            JOptionPane.showMessageDialog(null, "La conexion fue cerrada");
+                JOptionPane.showMessageDialog(null, "La conexión fue cerrada");
             }
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "No se pudo cerrar la conexion: "+ e.toString());
+            JOptionPane.showMessageDialog(null, "No se pudo cerrar la conexión: " + e.toString());
         }
     }
-    
-    
-    
-    
-    
-    
 }
